4afe2f6d7f5bd47321d14f5a6dd53805263b7593
sepehra90 <sepehra90@gmail.com>
2023-11-19 19:52:46 +0330

    1.03 "list works now"

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 import PyQt5
 import sys
 import psutil
+import json
+from os import listdir
+from os.path import isfile, join
+import os
 
 addrs = psutil.net_if_addrs()
-
+configs = [f for f in listdir("Configs") if isfile(join("Configs", f))]
 
 # def get_adaptor_list:
 
@@ -18,6 +22,10 @@ ui.setupUi(MainWindow)
 for x in addrs.keys():
     ui.comboBox.addItem(x)
 ui.comboBox.setCurrentText("Wi-Fi")
+
+ui.list_dns.clear()
+for x in configs:
+    ui.list_dns.addItem(os.path.splitext(x)[0])
 
 MainWindow.show()
 sys.exit(app.exec_())
