0aaa74a3e6f01aa08eb3bf17fe5eaf48420f07f8
sepehra90 <sepehra90@gmail.com>
2023-11-19 19:01:39 +0330

    v 1.01

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ from mainui import Ui_MainWindow
 from PyQt5 import QtCore, QtGui, QtWidgets
 import PyQt5
 import sys
+
+
+app = QtWidgets.QApplication(sys.argv)
+MainWindow = QtWidgets.QMainWindow()
+ui = Ui_MainWindow()
+ui.setupUi(MainWindow)
+MainWindow.show()
+sys.exit(app.exec_())
