05782ddc6aee65125bcc7b33262a2c1579b18c59
sepehra90 <sepehra90@gmail.com>
2023-11-19 19:17:56 +0330

    1.02 "added combobox"

--- main.py
+++ main.py
@@ -2,11 +2,22 @@ from mainui import Ui_MainWindow
 from PyQt5 import QtCore, QtGui, QtWidgets
 import PyQt5
 import sys
+import psutil
+
+addrs = psutil.net_if_addrs()
+
+
+# def get_adaptor_list:
 
 
 app = QtWidgets.QApplication(sys.argv)
 MainWindow = QtWidgets.QMainWindow()
 ui = Ui_MainWindow()
 ui.setupUi(MainWindow)
+
+for x in addrs.keys():
+    ui.comboBox.addItem(x)
+ui.comboBox.setCurrentText("Wi-Fi")
+
 MainWindow.show()
 sys.exit(app.exec_())
