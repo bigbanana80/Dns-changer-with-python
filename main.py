9eea56cb902b9fa49fc55593c967294c49a12ef8
sepehra90 <sepehra90@gmail.com>
2023-11-20 18:08:15 +0330

    1.04 "can now save files"

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import token
 from mainui import Ui_MainWindow
 from PyQt5 import QtCore, QtGui, QtWidgets
 import PyQt5
@@ -8,12 +9,24 @@ from os import listdir
 from os.path import isfile, join
 import os
 
+# variables that i should i not touch (prob) as they are for startup
 addrs = psutil.net_if_addrs()
 configs = [f for f in listdir("Configs") if isfile(join("Configs", f))]
 
-# def get_adaptor_list:
+
+class config:
+    def __init__(self, name: str, dns1: str, dns2: str) -> None:
+        self.name = name
+        self.dns1 = dns1
+        self.dns2 = dns2
+
+    def save_json(self):
+        values = {"name": self.name, "dns1": self.dns1, "dns2": self.dns2}
+        with open(f"Configs/{self.name}.json", "w") as file:
+            json.dump(values, file, indent=6)
 
 
+# startup section
 app = QtWidgets.QApplication(sys.argv)
 MainWindow = QtWidgets.QMainWindow()
 ui = Ui_MainWindow()
